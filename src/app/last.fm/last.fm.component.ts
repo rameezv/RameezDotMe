f4d06965d668b85dd8ed06c70cd5c7d57eb398a9
rameezv <rameez@bigtreeworld.com>
2019-03-04 20:29:17 -0500

    More last.fm building

--- src/app/last.fm/last.fm.component.ts
+++ src/app/last.fm/last.fm.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LastFmService } from './last.fm.service';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { LastFmTrack } from './interfaces';
 
 @Component({
@@ -10,10 +11,18 @@ import { LastFmTrack } from './interfaces';
 })
 export class LastFmComponent implements OnInit {
   recentTracks$: Observable<LastFmTrack[]>;
+  latestTrack$: Observable<LastFmTrack>;
+  isNowPlaying$: Observable<boolean>;
 
   constructor(private lastFmService: LastFmService) {}
   ngOnInit(): void {
     this.recentTracks$ = this.lastFmService.recentTracks$;
+    this.latestTrack$ = this.recentTracks$.pipe(
+      map(tracks => tracks ? tracks[0] : null)
+    );
+    this.isNowPlaying$ = this.latestTrack$.pipe(
+      map(track => track['@attr'] ? track['@attr'].nowplaying === 'true' : false)
+    );
     this.lastFmService.init();
   }
 }
