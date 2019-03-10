1d9113c6625df996ddd756f49f1db6aa1c2524b6
rameezv <rameez@bigtreeworld.com>
2019-03-10 16:39:14 -0400

    Add email button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFacebookSquare, faTwitterSquare, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 import { AppComponent } from './app.component';
 import { LastFmModule } from './last.fm/last.fm.module';
 
 const materialModules = [
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule
 ];
 
 @NgModule({
@@ -27,6 +29,6 @@ const materialModules = [
 })
 export class AppModule {
   constructor() {
-    library.add(faFacebookSquare, faTwitterSquare, faInstagram, faLinkedin);
+    library.add(faFacebookSquare, faTwitterSquare, faInstagram, faLinkedin, faEnvelopeSquare);
   }
 }
