2faf5ac2fbf5972a1395a26f70b4f2cf6498bc83
Rameez Virji <rameezv@Rameezs-Macbook-Pro.local>
2023-07-21 23:33:02 -0600

    Migrate to newer fontawesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faFacebookSquare, faTwitterSquare, faInstagram, faLinkedin, faGithubSquare, faGitlab, faSpotify, faYoutubeSquare, faApple } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 import { LastFmScrobblesModule } from 'angular-last-fm-scrobbles';
@@ -44,7 +44,7 @@ const materialModules = [
     bootstrap: [AppComponent]
 })
 export class AppModule {
-  constructor() {
-    library.add(faFacebookSquare, faTwitterSquare, faInstagram, faLinkedin, faEnvelopeSquare, faGithubSquare, faGitlab, faSpotify, faYoutubeSquare, faApple);
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebookSquare, faTwitterSquare, faInstagram, faLinkedin, faEnvelopeSquare, faGithubSquare, faGitlab, faSpotify, faYoutubeSquare, faApple);
   }
 }
